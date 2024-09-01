baa5ec73a61260f52d5e078aa31c8a4ba670c0a6
KAMBALENGUNUNU <kambalengununudaniel@gmail.com>
2024-09-01 15:53:48 +0200

    first commit

---
+++ AdSmartContracts.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AdSmartContracts {
+    struct Campaign {
+        address advertiser;
+        uint256 budget;
+        uint256 costPerClick;
+        uint256 costPerImpression;
+        uint256 totalClicks;
+        uint256 totalImpressions;
+        uint256 startTime;
+        uint256 endTime;
+        bool active;
+    }
